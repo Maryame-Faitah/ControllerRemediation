4505335ef1bb4844575fce4cca2f31f6a6d032c8
Maryame <faitah.maryame@gmail.com>
2020-05-28 11:07:47 +0200

    retourner view avec controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         //Retourner une view grace au controller
-        
+        return view('post');
     }
 
     /**
---
+++ resources/views/post.blade.php
@@ -0,0 +1,1 @@
+<h1>View post => test</h1>
